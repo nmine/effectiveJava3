b0f0305753a4b8d25dd215b61d3767e4b557be48
Nicolas Mine <nicolas.mine11@gmail.com>
2018-03-01 01:31:03 +0100

    test jenkins submodule

---
+++ submodule2/src/test/java/TestSubModule2.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSubModule2 {
+
+    @Test
+    public void test_sub_module_2() {
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test_sub_module_2_2() {
+        Assert.assertEquals(1,1);
+    }
+}
